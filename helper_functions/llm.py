3c79308ed64ffea4b3b07c992391b4b0f6d6aa4b
Kah Sha ONG <ong_kah_sha@htx.gov.sg>
2025-08-11 16:46:09 +0800

    keys

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -1,31 +1,41 @@
 import os
+
+import streamlit as st
+import tiktoken
 from dotenv import load_dotenv
 from openai import OpenAI
-import tiktoken
 
-
-load_dotenv('.env')
+if load_dotenv(".env"):
+    OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+else:
+    OPENAI_KEY = st.secrets["OPENAI_API_KEY"]
 
 # Pass the API Key to the OpenAI Client
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key=OPENAI_KEY)
 
-def get_embedding(input, model='text-embedding-3-small'):
-    response = client.embeddings.create(
-        input=input,
-        model=model
-    )
+
+def get_embedding(input, model="text-embedding-3-small"):
+    response = client.embeddings.create(input=input, model=model)
     return [x.embedding for x in response.data]
 
 
 # This is the "Updated" helper function for calling LLM
-def get_completion(prompt, model="gpt-4o-mini", temperature=0, top_p=1.0, max_tokens=1024, n=1, json_output=False):
+def get_completion(
+    prompt,
+    model="gpt-4o-mini",
+    temperature=0,
+    top_p=1.0,
+    max_tokens=1024,
+    n=1,
+    json_output=False,
+):
     if json_output == True:
-      output_json_structure = {"type": "json_object"}
+        output_json_structure = {"type": "json_object"}
     else:
-      output_json_structure = None
+        output_json_structure = None
 
     messages = [{"role": "user", "content": prompt}]
-    response = client.chat.completions.create( #originally was openai.chat.completions
+    response = client.chat.completions.create(  # originally was openai.chat.completions
         model=model,
         messages=messages,
         temperature=temperature,
@@ -38,14 +48,16 @@ def get_completion(prompt, model="gpt-4o-mini", temperature=0, top_p=1.0, max_to
 
 
 # Note that this function directly take in "messages" as the parameter.
-def get_completion_by_messages(messages, model="gpt-4o-mini", temperature=0, top_p=1.0, max_tokens=1024, n=1):
+def get_completion_by_messages(
+    messages, model="gpt-4o-mini", temperature=0, top_p=1.0, max_tokens=1024, n=1
+):
     response = client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=temperature,
         top_p=top_p,
         max_tokens=max_tokens,
-        n=1
+        n=1,
     )
     return response.choices[0].message.content
 
@@ -53,11 +65,11 @@ def get_completion_by_messages(messages, model="gpt-4o-mini", temperature=0, top
 # This function is for calculating the tokens given the "message"
 # ⚠️ This is simplified implementation that is good enough for a rough estimation
 def count_tokens(text):
-    encoding = tiktoken.encoding_for_model('gpt-4o-mini')
+    encoding = tiktoken.encoding_for_model("gpt-4o-mini")
     return len(encoding.encode(text))
 
 
 def count_tokens_from_message(messages):
-    encoding = tiktoken.encoding_for_model('gpt-4o-mini')
-    value = ' '.join([x.get('content') for x in messages])
+    encoding = tiktoken.encoding_for_model("gpt-4o-mini")
+    value = " ".join([x.get("content") for x in messages])
     return len(encoding.encode(value))
