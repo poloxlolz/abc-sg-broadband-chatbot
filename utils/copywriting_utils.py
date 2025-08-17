16028c6c5e2e4eb8a6e917fbd5aadd2d6cfec3da
Kah Sha ONG <ong_kah_sha@htx.gov.sg>
2025-08-17 21:31:31 +0800

    fix typo

--- utils/copywriting_utils.py
+++ utils/copywriting_utils.py
@@ -13,7 +13,7 @@ class Copies(str, Enum):
     CHAT_INPUT_PLACEHOLDER = "Ask anything"
     TRY_AGAIN = "Sorry, unexpected issue encountered, please try again later!"
     DISCLAIMER = """
-                <span style="color:#FF82B2">**IMPORTANT NOTICE:**</span>. This web application is a prototype developed for <span style="color:#FF82B2">**educational purposes only.**</span> The information provided here is <span style="color:#FF82B2">**NOT intended for real-world usage**</span> and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+                <span style="color:#FF82B2">**IMPORTANT NOTICE:**</span> This web application is a prototype developed for <span style="color:#FF82B2">**educational purposes only.**</span> The information provided here is <span style="color:#FF82B2">**NOT intended for real-world usage**</span> and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
 
                 <span style="color:#FF82B2">**Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.**</span>
 
